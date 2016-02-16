81adad41b26cd926c5244fa9cc84ff9f7db03f82
SergPronenko <pronenko_sn@i.ua>
2016-02-16 19:02:56 +0200

    MyLinkedList

--- MyLinkedList/src/com/company/Main.java
+++ MyLinkedList/src/com/company/Main.java
@@ -1,17 +1,29 @@
 package com.company;
 
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        List<String> list = (List<String>) new MyLinkedList<>();
-        list.add("0");
-        list.add("1");
-        list.add("100");
+      //  MyLinkedList<String> list =  new MyLinkedList<>();
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.addFirst(1);
+        list.addLast(5);
+        list.addFirst(10);
+        list.addFirst(2);
+        list.addLast(8);
+        list.addFirst(19);
 
+        for (Object i : list) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+        System.out.println("======================");
+        list.removeFirst();
+        list.removeLast();
 
+        for (Object i : list) {
+            System.out.print(i + ", ");
+        }
 
     }
 }
--- MyLinkedList/src/com/company/MyLinkedList.java
+++ MyLinkedList/src/com/company/MyLinkedList.java
@@ -1,12 +1,34 @@
 package com.company;
 
+import java.util.Iterator;
+
 /**
  * Created by admin on 13.02.2016.
  */
-public class MyLinkedList<T>  {
-    transient Node<T>first;
-    transient Node<T>last;
-    transient int size = 0;
+public class MyLinkedList<T> implements Iterable {
+    private Node<T>first;
+    private Node<T>last;
+    private int size = 0;
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node curr = first;
+            @Override
+            public boolean hasNext() {
+                return curr !=null;
+            }
+
+            @Override
+            public T next() {
+                Node temp = curr;
+                curr = curr.next;
+                return (T)temp.value;
+            }
+        };
+    }
+
+
 
 
     private static class Node<T> {
@@ -14,64 +36,81 @@ public class MyLinkedList<T>  {
         private Node<T> next;
         private Node<T> prev;
 
+        public Node() {
+        }
+
         public Node(Object value, T next, Node<T> prev) {
             this.value = (T) value;
             this.next = (Node<T>) next;
             this.prev = prev;
         }
+
+
     }
 
 
-    public void addFirst(T t){
-        final Node<T> f = first;
-        final Node<T> newNode = new Node<>(null,t,f);
-        first = newNode;
-        if (f==null){
+    public void addFirst (T value) {
+
+        if (isEmpty()) {
+            Node newNode = new Node();
+            newNode.value = value;
             last = newNode;
-        }else {
-            f.prev =newNode;
-        }
-        size++;
-    }
-
-    public void addLast(T t){
-        final Node<T> l = last;
-        final Node<T> newNode = new Node<>(l,t,null);
-        last = newNode;
-        if (l==null){
+            first = last;
+        } else {
+            Node newNode = new Node(value ,first , null );
+            first.prev = newNode;
             first = newNode;
-        }else {
-            l.next =newNode;
+        }
+        ++size;
+    }
+
+    public void addLast (T value) {
+
+        if (isEmpty()) {
+
+            Node newNode = new Node();
+            newNode.value = value;
+            last  = newNode;
+            first = last;
+        } else {
+            Node previous = last;
+            last = new Node();
+            last.value = value;
+            previous.next = last;
+            last.prev = previous;
+
         }
         size++;
     }
 
-    private void removeRirst(Node<T> f){
-        final T value =f.value;
-        final Node<T> next = f.next;
-        f.value = null;
-        f.next = null;
-        first = next;
-        if (next == null){
-            last = null;
-        }else {
-            next.prev = null;
-        }
-        size--;
+    public boolean isEmpty(){
+        return (size == 0);
     }
-    private void  removeLast(Node<T> l){
-        final T value = l.value;
-        final Node<T>prev = l.prev;
-        l.value = null;
-        l.prev = null;
-        last = prev;
-        if (prev == null){
-            first = null;
-        }else {
-            prev.next = null;
-        }
-        size--;
 
+    public void removeFirst() {
+        if( size == 0 ){
+            return;
+        }else if (size == 1){
+            size = 0;
+            last = first = null;
+            return;
+        }
+        first = first.next;
+        first.prev = null ;
+        --size;
+
+    }
+    public void removeLast() {
+        if( size == 0 ){
+            return;
+        }else if (size == 1 ){
+            size = 0;
+            last = first = null;
+            return;
+        }
+        last = last.prev;
+        last.next = null;
+        size--;
     }
 
     Node<T> node(int index) {
@@ -89,15 +128,43 @@ public class MyLinkedList<T>  {
         }
     }
 
-    private void checkValumeIndex (int index, int seze){
-        if (index < 0 || index >= size){
-            throw new IndexOutOfBoundsException();
+//    private void checkValumeIndex (int index, int seze){
+//        if (index < 0 || index >= size){
+//            throw new IndexOutOfBoundsException();
+//        }
+//    }
+
+
+
+    public T get (int idx){
+
+        if (idx > size) {
+            throw new ArrayIndexOutOfBoundsException("Out of index");
         }
+
+        Node temp = first;
+
+
+        for (int i = 0 ; i < idx; i++) {
+            temp = temp.next;
+        }
+        return (T)temp.value;
     }
 
-    public T get (int index){
-        checkValumeIndex(index,size);
-        return node(index).value;
+
+    public void set(T item, int idx) {
+        if (idx > size) {
+            throw new ArrayIndexOutOfBoundsException("Out of index");
+        }
+        Node temp = first;
+
+        for (int i = 0 ; i < idx; i++) {
+            temp = temp.next;
+        }
+        temp.value = item;
     }
+
+
+
 
 }
